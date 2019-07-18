dbe992f41c566a0571fe4a8aaa22ee2cd3474af6
luchenchen <luchenchen@wowkai.cn>
2019-07-18 16:26:05 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import hell from '@/components/HelloWorld.vue'
+import home from '@/components/home.vue'
 Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [{
+    path: '/',
+    name: 'home',
+    component: home,
+    meta: {
+      title: 'test'
+    }
+  },{
     path: '/hell',
     name: 'hell',
     component: hell,
